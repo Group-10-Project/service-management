13fc3cc260e563921c76d37c1a425017482d2fb2
Harshet <d.harshet@nagarro.com>
2023-11-01 19:40:18 +0530

    Setup H2 for Unit Testing

--- src/test/java/com/review/servicemanagement/services/categoryServiceTest.java
+++ src/test/java/com/review/servicemanagement/services/categoryServiceTest.java
@@ -1,20 +1,30 @@
 package com.review.servicemanagement.services;
 
+
 import com.review.servicemanagement.dto.ResponseCategoryDTO;
 import com.review.servicemanagement.dto.createCategoryDTO;
 import com.review.servicemanagement.exceptions.DuplicateValueException;
 import com.review.servicemanagement.models.CategoryModel;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+//@RunWith(SpringRunner.class)
+//@DataJpaTest
+//@ContextConfiguration(classes= ServiceManagementApplicationTests.class)
 @SpringBootTest
-//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@ActiveProfiles("test")
 public class categoryServiceTest {
 
     String categoryName="Category_1";
